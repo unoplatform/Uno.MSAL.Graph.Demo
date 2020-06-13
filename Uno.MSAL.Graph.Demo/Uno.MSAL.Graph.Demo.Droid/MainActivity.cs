6c0eb1ff18a9420d8693c36bf2f4f5a62dadbfa6
Carl de Billy <carl.debilly@gmail.com>
2020-06-13 11:55:59 -0400

    Moved MsalActivity into its own file

--- Uno.MSAL.Graph.Demo/Uno.MSAL.Graph.Demo.Droid/MainActivity.cs
+++ Uno.MSAL.Graph.Demo/Uno.MSAL.Graph.Demo.Droid/MainActivity.cs
@@ -21,14 +21,5 @@ namespace Uno.MSAL.Graph.Demo.Droid
             AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
         }
     }
-
-    [Activity]
-    [IntentFilter(new[] { Intent.ActionView },
-        Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
-        DataHost = "Uno.MSAL.Graph.Demo",
-        DataScheme = "msauth")]
-    public class MsalActivity : BrowserTabActivity
-    {
-    }
 }
 
---
+++ Uno.MSAL.Graph.Demo/Uno.MSAL.Graph.Demo.Droid/MsalActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Content;
+using Microsoft.Identity.Client;
+
+namespace Uno.MSAL.Graph.Demo.Droid
+{
+    [Activity]
+    [IntentFilter(new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
+        DataHost = "Uno.MSAL.Graph.Demo",
+        DataScheme = "msauth")]
+    public class MsalActivity : BrowserTabActivity
+    {
+    }
+}
